940dbe4e1a5c982742b825f968632b2c5c7b54ed
Vesa Laakso <482561+valscion@users.noreply.github.com>
2021-03-14 14:30:56 +0200

    Calculate diagonal winners

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -125,12 +125,40 @@ impl GameArea {
   }
 
   fn diagonal_from_top_left_to_bottom_right(&self) -> Option<Player> {
-    // TODO: Calculate a potential winner diagonally!
+    for y in 0..(self.height - 4) {
+      for x in 0..(self.width - 4) {
+        if let Some(&first) = self.games.get(&format!("{},{}", x, y)) {
+          let next_cells = vec![
+            self.games.get(&format!("{},{}", x + 1, y + 1)),
+            self.games.get(&format!("{},{}", x + 2, y + 2)),
+            self.games.get(&format!("{},{}", x + 3, y + 3)),
+            self.games.get(&format!("{},{}", x + 4, y + 4)),
+          ];
+          if next_cells.iter().all(|&item| item == Some(&first)) {
+            return Some(first);
+          }
+        }
+      }
+    }
     None
   }
 
   fn diagonal_from_top_right_to_bottom_left(&self) -> Option<Player> {
-    // TODO: Calculate a potential winner diagonally!
+    for y in 0..(self.height - 4) {
+      for x in 4..self.width {
+        if let Some(&first) = self.games.get(&format!("{},{}", x, y)) {
+          let next_cells = vec![
+            self.games.get(&format!("{},{}", x - 1, y + 1)),
+            self.games.get(&format!("{},{}", x - 2, y + 2)),
+            self.games.get(&format!("{},{}", x - 3, y + 3)),
+            self.games.get(&format!("{},{}", x - 4, y + 4)),
+          ];
+          if next_cells.iter().all(|&item| item == Some(&first)) {
+            return Some(first);
+          }
+        }
+      }
+    }
     None
   }
 }
@@ -318,6 +346,68 @@ mod tests {
          ...x..\n\
          ...x..\n\
          ...x..",
+      )
+      .winner()
+      .unwrap(),
+      Player::Cross
+    );
+  }
+
+  #[test]
+  fn test_winner_diagonally_down_from_left_to_right() {
+    assert_eq!(
+      GameArea::from_template(
+        "o.....\n\
+         .o....\n\
+         ..o...\n\
+         ...o..\n\
+         ....o.\n\
+         ......",
+      )
+      .winner()
+      .unwrap(),
+      Player::Naught
+    );
+
+    assert_eq!(
+      GameArea::from_template(
+        "......\n\
+         .x....\n\
+         ..x...\n\
+         ...x..\n\
+         ....x.\n\
+         .....x",
+      )
+      .winner()
+      .unwrap(),
+      Player::Cross
+    );
+  }
+
+  #[test]
+  fn test_winner_diagonally_down_from_right_to_left() {
+    assert_eq!(
+      GameArea::from_template(
+        ".....o\n\
+         ....o.\n\
+         ...o..\n\
+         ..o...\n\
+         .o....\n\
+         ......",
+      )
+      .winner()
+      .unwrap(),
+      Player::Naught
+    );
+
+    assert_eq!(
+      GameArea::from_template(
+        "......\n\
+         ....x.\n\
+         ...x..\n\
+         ..x...\n\
+         .x....\n\
+         x.....",
       )
       .winner()
       .unwrap(),
