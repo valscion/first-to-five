4a6d68b45d12a2bc4bd83b8cf79940943e5bfa38
Vesa Laakso <482561+valscion@users.noreply.github.com>
2021-03-14 14:57:02 +0200

    Growable area

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -8,8 +8,10 @@ pub enum Player {
 }
 
 pub struct GameArea {
-  width: u128,
-  height: u128,
+  left: i128,
+  top: i128,
+  right: i128,
+  bottom: i128,
   /// The values selected stored in a HashMap where the keys
   /// are specifically formatted with "X,Y" formatted strings.
   /// For example: { "100,50" => Player::Naught } would mean that
@@ -18,10 +20,12 @@ pub struct GameArea {
 }
 
 impl GameArea {
-  pub fn new(width: u128, height: u128) -> Self {
+  pub fn new(width: i128, height: i128) -> Self {
     GameArea {
-      width,
-      height,
+      left: 0,
+      top: 0,
+      right: width,
+      bottom: height,
       games: HashMap::default(),
     }
   }
@@ -42,8 +46,8 @@ impl GameArea {
   /// ```
   pub fn from_template(template: &'static str) -> Self {
     let lines: Vec<&str> = template.split("\n").collect();
-    let height = lines.len() as u128;
-    let width = lines[0].len() as u128;
+    let height = lines.len() as i128;
+    let width = lines[0].len() as i128;
     let mut area = GameArea::new(width, height);
     for (row, line) in lines.iter().enumerate() {
       let row_width = line.chars().count();
@@ -58,8 +62,8 @@ impl GameArea {
       for (column, character) in line.chars().enumerate() {
         match character {
           '.' => { /* blank, do nothing */ }
-          'x' => area.mark(Player::Cross, column as u128, row as u128),
-          'o' => area.mark(Player::Naught, column as u128, row as u128),
+          'x' => area.mark(Player::Cross, column as i128, row as i128),
+          'o' => area.mark(Player::Naught, column as i128, row as i128),
           _ => panic!("Invalid template character: '{}'", character),
         }
       }
@@ -67,14 +71,26 @@ impl GameArea {
     area
   }
 
-  pub fn mark(&mut self, player: Player, x: u128, y: u128) {
+  pub fn mark(&mut self, player: Player, x: i128, y: i128) {
+    if x < self.left {
+      self.left = x;
+    } else if x >= self.right {
+      self.right = x + 1;
+    }
+
+    if y < self.top {
+      self.top = y;
+    } else if y >= self.bottom {
+      self.bottom = y + 1;
+    }
+
     let key = format!("{},{}", x, y);
     self.games.insert(key, player);
   }
 
   pub fn winner(&self) -> Option<Player> {
-    for y in 0..self.height {
-      for x in 0..self.width {
+    for y in self.top..self.bottom {
+      for x in self.left..self.right {
         if let Some(&first) = self.games.get(&format!("{},{}", x, y)) {
           let horizontal_next_cells = [
             self.games.get(&format!("{},{}", x + 1, y)),
@@ -112,16 +128,14 @@ impl GameArea {
             return Some(first);
           }
 
-          if x >= 4 {
-            let next_cells = [
-              self.games.get(&format!("{},{}", x - 1, y + 1)),
-              self.games.get(&format!("{},{}", x - 2, y + 2)),
-              self.games.get(&format!("{},{}", x - 3, y + 3)),
-              self.games.get(&format!("{},{}", x - 4, y + 4)),
-            ];
-            if next_cells.iter().all(|&item| item == Some(&first)) {
-              return Some(first);
-            }
+          let next_cells = [
+            self.games.get(&format!("{},{}", x - 1, y + 1)),
+            self.games.get(&format!("{},{}", x - 2, y + 2)),
+            self.games.get(&format!("{},{}", x - 3, y + 3)),
+            self.games.get(&format!("{},{}", x - 4, y + 4)),
+          ];
+          if next_cells.iter().all(|&item| item == Some(&first)) {
+            return Some(first);
           }
         }
       }
@@ -133,13 +147,13 @@ impl GameArea {
 impl fmt::Display for GameArea {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     write!(f, "⌜")?;
-    for _ in 0..self.width {
+    for _ in self.left..self.right {
       write!(f, "⎺")?;
     }
     writeln!(f, "⌝")?;
-    for y in 0..self.height {
+    for y in self.top..self.bottom {
       write!(f, "|")?;
-      for x in 0..self.width {
+      for x in self.left..self.right {
         let key = format!("{},{}", x, y);
         match self.games.get(&key) {
           Some(Player::Cross) => write!(f, "x")?,
@@ -150,7 +164,7 @@ impl fmt::Display for GameArea {
       writeln!(f, "|")?;
     }
     write!(f, "⌞")?;
-    for _ in 0..self.width {
+    for _ in self.left..self.right {
       write!(f, "⎽")?;
     }
     write!(f, "⌟")
@@ -224,6 +238,69 @@ mod tests {
        |o o|\n\
        | x |\n\
        ⌞⎽⎽⎽⌟",
+    );
+  }
+
+  #[test]
+  fn test_area_enlargening() {
+    let mut area = GameArea::new(0, 0);
+    // First the area should be empty
+    assert_area_formatted_to(
+      &area,
+      "⌜⌝\n\
+       ⌞⌟",
+    );
+    // When we add our first mark, the area should get to become 1x1 sized
+    area.mark(Player::Cross, 0, 0);
+    assert_area_formatted_to(
+      &area,
+      "⌜⎺⌝\n\
+       |x|\n\
+       ⌞⎽⌟",
+    );
+    // Then we push it one to the right with 1, it should grow
+    area.mark(Player::Naught, 1, 0);
+    assert_area_formatted_to(
+      &area,
+      "⌜⎺⎺⌝\n\
+       |xo|\n\
+       ⌞⎽⎽⌟",
+    );
+
+    // Then let's go more to the left of the original left side and watch the area grow
+    area.mark(Player::Cross, -3, 0);
+    assert_area_formatted_to(
+      &area,
+      "⌜⎺⎺⎺⎺⎺⌝\n\
+       |x  xo|\n\
+       ⌞⎽⎽⎽⎽⎽⌟",
+    );
+
+    // And then more to the top than originally was
+    area.mark(Player::Naught, 0, -4);
+    assert_area_formatted_to(
+      &area,
+      "⌜⎺⎺⎺⎺⎺⌝\n\
+       |   o |\n\
+       |     |\n\
+       |     |\n\
+       |     |\n\
+       |x  xo|\n\
+       ⌞⎽⎽⎽⎽⎽⌟",
+    );
+    // And finally more to the bottom than there was space
+    area.mark(Player::Cross, 1, 2);
+    assert_area_formatted_to(
+      &area,
+      "⌜⎺⎺⎺⎺⎺⌝\n\
+       |   o |\n\
+       |     |\n\
+       |     |\n\
+       |     |\n\
+       |x  xo|\n\
+       |     |\n\
+       |    x|\n\
+       ⌞⎽⎽⎽⎽⎽⌟",
     );
   }
 
