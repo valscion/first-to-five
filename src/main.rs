ea120c2f47e66ea526d7b51cc7dc021ece565056
Vesa Laakso <482561+valscion@users.noreply.github.com>
2021-03-15 17:29:18 +0200

    Fiddle around with iterators for the proper game

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,47 @@
 mod rules;
 use rules::{GameArea, Player};
 
+const FIRST_TO_PLAY: rules::Player = Player::Cross;
+
 fn main() {
     let mut area = GameArea::default();
-    // Add one play by Naught
-    area.mark(Player::Naught, 0, 0);
-    // Mark a diagonal line in the middle of the game field
-    for x in 3..8 {
-        let y = x - 3;
-        area.mark(Player::Cross, x, y);
+    let naught_plays = [(0i128, 0i128), (1, 0), (4, 0), (3, 0), (2, 0)]
+        .iter()
+        .map(|(x, y)| (x, y, Player::Naught))
+        .collect::<Vec<_>>();
+    let cross_plays = [(2i128, 1), (3, 2), (6, 5), (4, 3), (5, 4)]
+        .iter()
+        .map(|(x, y)| (x, y, Player::Cross))
+        .collect::<Vec<_>>();
+
+    let plays = if FIRST_TO_PLAY == Player::Naught {
+        naught_plays.iter().zip(cross_plays.iter())
+    } else {
+        cross_plays.iter().zip(naught_plays.iter())
+    };
+    for (round, ((x1, y1, player1), (x2, y2, player2))) in plays.enumerate() {
+        println!("# Round {} starts!\n", round + 1);
+
+        area.mark(*player1, **x1, **y1);
+        print_area(&area);
+        if area.winner().is_some() {
+            break;
+        }
+
+        area.mark(*player2, **x2, **y2);
+        print_area(&area);
+        if area.winner().is_some() {
+            break;
+        }
+        println!();
     }
+
+    println!("\n\nGame has ended!");
+}
+
+fn print_area(area: &GameArea) {
     println!("{}", area);
-    match area.winner() {
-        Some(Player::Naught) => println!("Naught has won!"),
-        Some(Player::Cross) => println!("Cross has won!"),
-        None => println!("No winner yet..."),
+    if let Some(player) = area.winner() {
+        println!("{:?} has won!", player);
     }
 }
