5ffd0bc66f60e5918688baac49f89838199ad519
Vesa Laakso <482561+valscion@users.noreply.github.com>
2021-03-28 15:53:46 +0300

    Simplify console printing with the help of itertools

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod rules;
+use itertools::Itertools;
 use rules::{GameArea, Player};
 
 const FIRST_TO_PLAY: rules::Player = Player::Cross;
@@ -9,35 +10,25 @@ use piston_window::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut area = GameArea::default();
-    let naught_plays = [(0i128, 0i128), (1, 0), (4, 0), (3, 0), (2, 0)]
-        .iter()
-        .map(|(x, y)| (x, y, Player::Naught))
-        .collect::<Vec<_>>();
-    let cross_plays = [(2i128, 1), (3, 2), (6, 5), (4, 3), (5, 4)]
-        .iter()
-        .map(|(x, y)| (x, y, Player::Cross))
-        .collect::<Vec<_>>();
+    let plays_one = [(0i128, 0i128), (1, 0), (4, 0), (3, 0), (2, 0)];
+    let plays_two = [(2i128, 1), (3, 2), (6, 5), (4, 3), (5, 4)];
 
-    let plays = if FIRST_TO_PLAY == Player::Naught {
-        naught_plays.iter().zip(&cross_plays)
-    } else {
-        cross_plays.iter().zip(&naught_plays)
-    };
-    for (round, ((x1, y1, player1), (x2, y2, player2))) in plays.enumerate() {
-        println!("# Round {} starts!\n", round + 1);
-
-        area.mark(*player1, **x1, **y1)?;
+    let plays = plays_one.iter().interleave(&plays_two);
+    for (i, (x, y)) in plays.enumerate() {
+        let player = if i % 2 == 0 {
+            !FIRST_TO_PLAY
+        } else {
+            FIRST_TO_PLAY
+        };
+        area.mark(player, *x, *y)?;
         print_area(&area);
         if area.winner().is_some() {
+            println!(
+                "Longest line: {:?}",
+                area.longest_consecutive_line(*x, *y).unwrap()
+            );
             break;
         }
-
-        area.mark(*player2, **x2, **y2)?;
-        print_area(&area);
-        if area.winner().is_some() {
-            break;
-        }
-        println!();
     }
 
     let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
