764d7c8fee6a3f07160750a3c4c8581027597eb0
Vesa Laakso <482561+valscion@users.noreply.github.com>
2021-03-28 16:00:14 +0300

    Extract parts of main to functions

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,15 @@ extern crate piston_window;
 
 use piston_window::*;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let mut area = GameArea::default();
+    example_play(&mut area);
+    start_gui(&area);
+
+    println!("\n\nGame has ended!");
+}
+
+fn example_play(area: &mut GameArea) {
     let plays_one = [(0i128, 0i128), (1, 0), (4, 0), (3, 0), (2, 0)];
     let plays_two = [(2i128, 1), (3, 2), (6, 5), (4, 3), (5, 4)];
     let first_to_play = if random::<bool>() {
@@ -24,7 +31,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         } else {
             first_to_play
         };
-        area.mark(player, *x, *y)?;
+        area.mark(player, *x, *y).expect("Nobody should've won yet");
         print_area(&area);
         if area.winner().is_some() {
             println!(
@@ -34,7 +41,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             break;
         }
     }
+}
 
+fn print_area(area: &GameArea) {
+    println!("{}", area);
+    if let Some(player) = area.winner() {
+        println!("{:?} has won!", player);
+    }
+}
+
+fn start_gui(_area: &GameArea) {
     let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
         .exit_on_esc(true)
         .build()
@@ -49,15 +65,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 graphics,
             );
         });
-    }
-
-    println!("\n\nGame has ended!");
-    Ok(())
-}
-
-fn print_area(area: &GameArea) {
-    println!("{}", area);
-    if let Some(player) = area.winner() {
-        println!("{:?} has won!", player);
     }
 }
